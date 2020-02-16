5a511a0d10e0cc77f9ec19789c19ebbc58ee5fdf
IvanAlieksieienko <i.alieksieienko@student.csn.khai.edu>
2020-02-16 10:43:01 +0200

    Added images to products on xml page

--- Views/xml.php
+++ Views/xml.php
@@ -2,12 +2,13 @@
 <div class="store">
     <?php
 
-    $laptops = simplexml_load_file("xml.xml");
+    $laptops = simplexml_load_file("../xml/xml.xml");
     // $laptops = new SimpleXMLElement($xml);
 
     foreach ($laptops->laptop as $laptop) {
-        printf("<div class=\"laptops\">
+        printf("<div class=\"laptops\"> <img class=\"img_product\" src=\"../xml/img/%s\">
         <p>Name: %s <br>Price: %s <br>Serial: %s <br>Year: %s <br>Memory: %s <br>OS: %s <br>RAM: %s</p></div>",
+        $laptop->Image,
         $laptop->Name,
         $laptop->Price,
         $laptop->SerialNumber,
